fcc7b8295b25e8e648acdf98009e4bd0a7c584d7
the-Dust <dust_@inbox.ru>
2017-12-18 20:20:50 +0400

    Добавлены модульные тесты.

---
+++ UnitTests/ParserTests.cs
@@ -0,0 +1,131 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using StatMethods.Parsers;
+
+
+namespace UnitTests
+{
+    [TestClass]
+    public class ParserTests
+    {
+        [TestMethod]
+        public void CanParseValidArray()
+        {
+            //Arrange
+            DataParser parser = new DataParser();
+
+            string inputString = "  34  2,41 4.936 23 300    50    ";
+            double[] reference = { 34, 2.41, 4.936, 23, 300, 50};
+
+            //Action
+            double[] parsedArray = parser.ParseArray(inputString);
+
+            //Assert
+            CollectionAssert.AreEqual(reference, parsedArray);
+            Assert.AreEqual("", parser.ValidationMessage);
+        }
+        
+        [TestMethod]
+        public void GiveMessageWithInvalidInput()
+        {
+            //Arrange
+            DataParser parser = new DataParser();
+
+            string inputString1 = "  текст  2,41 ?;№ 23 300    50    ";
+            string inputString2 = "    ";
+            string inputString3 = null;
+            string inputString4 = "111";
+
+            string refMessage1 = "В позиции (позициях): 1, 3 введены данные, " +
+                       Environment.NewLine + "не являющиеся числовыми либо не соответствующие диапазону. ";
+            string refMessage2 = "Вместо массива данных введена пустая строка. ";
+
+            string refMessage3 = "Вместо массива данных введена пустая строка. ";
+
+            string refMessage4 = "Массив не может состоять из одного элемента. ";
+
+            //Action
+            double[] parsedArray1 = parser.ParseArray(inputString1);
+            string message1 = parser.ValidationMessage;
+
+            double[] parsedArray2 = parser.ParseArray(inputString2);
+            string message2 = parser.ValidationMessage;
+
+            double[] parsedArray3 = parser.ParseArray(inputString3);
+            string message3 = parser.ValidationMessage;
+
+            double[] parsedArray4 = parser.ParseArray(inputString4);
+            string message4 = parser.ValidationMessage;
+
+            //Assert
+            CollectionAssert.AreEqual(null, parsedArray1);
+            Assert.IsTrue(message1.Contains(refMessage1));
+            
+            CollectionAssert.AreEqual(null, parsedArray2);
+            Assert.IsTrue(message2.Contains(refMessage2));
+
+            CollectionAssert.AreEqual(null, parsedArray3);
+            Assert.IsTrue(message3.Contains(refMessage3));
+
+            CollectionAssert.AreEqual(null, parsedArray4);
+            Assert.IsTrue(message4.Contains(refMessage4));
+        }
+
+        [TestMethod]
+        public void CanParseValidParameter()
+        {
+            //Arrange
+            DataParser parser = new DataParser();
+
+            string inputString = "4";
+            int reference = 4;
+
+            //Action
+            int parsedParameter = parser.ParseParameter(inputString, 10);
+
+            //Assert
+            Assert.AreEqual(reference, parsedParameter);
+            Assert.AreEqual("", parser.ValidationMessage);
+        }
+
+        [TestMethod]
+        public void GiveMessageWithInvalidParameter()
+        {
+            //Arrange
+            DataParser parser = new DataParser();
+
+            string inputString1 = "%$#4";
+            int reference = -1;
+            string refMessage1 = "Введенный параметр не является числовым значением";
+
+            string inputString2 = "1";
+            
+            string refMessage2 = "Размер параметра не может быть меньше 2 и больше размера массива. ";
+
+            string inputString3 = "20";
+            
+            string refMessage3 = "Размер параметра не может быть меньше 2 и больше размера массива. ";
+
+            //Action
+            int parsedParameter1 = parser.ParseParameter(inputString1, 10);
+            string message1 = parser.ValidationMessage;
+
+            int parsedParameter2 = parser.ParseParameter(inputString2, 10);
+            string message2 = parser.ValidationMessage;
+
+            int parsedParameter3 = parser.ParseParameter(inputString3, 10);
+            string message3 = parser.ValidationMessage;
+
+
+            //Assert
+            Assert.AreEqual(reference, parsedParameter1);
+            Assert.IsTrue(message1.Contains(refMessage1));
+
+            Assert.AreEqual(reference, parsedParameter2);
+            Assert.IsTrue(message2.Contains(refMessage2));
+
+            Assert.AreEqual(reference, parsedParameter3);
+            Assert.IsTrue(message3.Contains(refMessage3));
+        }
+    }
+}
---
+++ UnitTests/StatTests.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using StatMethods.MedianFilters;
+using StatMethods.Winsorizators;
+
+namespace UnitTests
+{
+    [TestClass]
+    public class StatTests
+    {
+        [TestMethod]
+        public void CanGetValidWinsorization()
+        {
+            //Arrange
+            Winsorizator wins = new Winsorizator();
+
+            double[] input1 = { 2, 3, 17, 6, 1 };
+            int m1 = 2;
+            double reference1 = 5.8;
+
+            double[] input2 = { 17, 38, 9, 63, 41, 15 };
+            int m2 = 4;
+            double reference2 = 31.75;
+
+            double[] input3 = { 7, 82, 33, 33, 33, 1, 56 };
+            int m3 = 2;
+            double reference3 = 35;
+
+            //Action
+            double result1 = wins.FindWinsorizedMedium(input1, m1);
+
+            double result2 = wins.FindWinsorizedMedium(input2, m2);
+
+            double result3 = wins.FindWinsorizedMedium(input3, m3);
+
+            //Assert
+            Assert.AreEqual(reference1, result1, 0.0001);
+            Assert.AreEqual(reference2, result2, 0.0001);
+            Assert.AreEqual(reference3, result3, 0.0001);
+        }
+
+        [TestMethod]
+        public void CanGetValidMedianFiltration()
+        {
+            //Arrange
+            MedianFilter filter = new MedianFilter();
+
+            double[] input1 = { 2, 80, 6, 3 };
+            int window1 = 3;
+            double[] reference1 = { 2,6,6,3};
+
+            double[] input2 = { 17, 38, 9, 63, 41, 15 };
+            int window2 = 4;
+            double[] reference2 = { 17, 17, 27.5, 39.5, 28, 28 };
+
+            double[] input3 = { 7, 100, 1, 33, 12, 1, 56 };
+            int window3 = 5;
+            double[] reference3 = { 7, 7, 12, 12, 12, 33, 56 };
+
+            //Action
+            double[] result1 = filter.GetMedianFiltration(input1, window1);
+
+            double[] result2 = filter.GetMedianFiltration(input2, window2);
+
+            double[] result3 = filter.GetMedianFiltration(input3, window3);
+
+            //Assert
+            CollectionAssert.AreEqual(reference1, result1);
+            CollectionAssert.AreEqual(reference2, result2);
+            CollectionAssert.AreEqual(reference3, result3);
+        }
+    }
+}
